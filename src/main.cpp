23f18689115c7775a6ba0ecdcef113d9a51a1e74
Patrick Nguyen <patrickanguyen1258@gmail.com>
2022-04-15 12:50:41 -0700

    Create dummy tasks

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,33 @@
 #include <Arduino.h>
+#include <FreeRTOS.h>
+
+#define ZBIN_CORE_VERSION "0.0.1"
+
+// These tasks will be removed in a later version
+// This is just show how tasks work in FreeRTOS
+void dummy_task1(void *params) {
+    while (1) {
+        log_i("Hello from DummyTask1");
+
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay for 1000 ms
+    }
+}
+
+void dummy_task2(void *params) {
+    while (1) {
+        log_i("Hello from DummyTask2");
+        vTaskDelay(5000 / portTICK_PERIOD_MS); // Delay for 5000 ms
+    }
+}
 
 void setup() {
-  // put your setup code here, to run once:
+    log_i("ZotBins Core Version: %s", ZBIN_CORE_VERSION);
+
+    // Create tasks to run
+    xTaskCreate(dummy_task1, "DummyTask1", 1024, nullptr, 1, nullptr);
+    xTaskCreate(dummy_task2, "DummyTask2", 1024, nullptr, 1, nullptr);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // Do nothing. Things will be done in tasks
 }
