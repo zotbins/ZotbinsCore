e03b11649fb2cdf7256c2f334d7bdae78eff930e
Emil Zublin <ejzublin@gmail.com>
2022-04-23 18:33:56 -0700

    Made small changes to the IWeight class docs
    Co-authored-by: Derek Skinner dskin344@gmail.com

--- src/Weight/IWeight.hpp
+++ src/Weight/IWeight.hpp
@@ -3,14 +3,19 @@
 
 #include <cstdint>
 
+/**
+ * @brief Interface (abstract) class for mock and real hardware weight sensor.
+ * Used to inherit the getWeight method in derived classes.
+ * 
+ */
 class IWeight 
 {
+
     /**
      * @brief Returns the weight of the sensor.
      * 
      * @return int32_t 
      */
-
     virtual int32_t getWeight() = 0;
 };
 
