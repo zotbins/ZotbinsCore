211f415591976c5643fd02cc5c0c2dc9e2cc2f97
Patrick Nguyen <41417417+patrickanguyen@users.noreply.github.com>
2022-04-27 11:15:27 -0700

    Setup unit tests (#17)

---
+++ test/test_desktop_weight/testWeight.cpp
@@ -0,0 +1,16 @@
+#include <unity.h>
+
+// TODO: Add more tests
+
+void test_always_pass()
+{
+    TEST_ASSERT_TRUE(true);
+}
+
+int main(int argc, char** argv)
+{
+    UNITY_BEGIN();
+    RUN_TEST(test_always_pass);
+    UNITY_END();
+    return 0;
+}
