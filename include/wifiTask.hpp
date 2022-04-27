ad723bb42505dfb69be80130d52c77330eee7152
Patrick Nguyen <patrickanguyen1258@gmail.com>
2022-04-26 23:51:27 -0700

    Added WiFi task

---
+++ include/wifiTask.hpp
@@ -0,0 +1,52 @@
+#ifndef WIFI_TASK_HPP
+#define WIFI_TASK_HPP
+
+#include "task.hpp"
+
+namespace Zotbins
+{
+    class WiFiTask : public Task
+    {
+    public:
+        WiFiTask();
+
+        /**
+         * @brief Start execution of WiFi Task
+         *
+         */
+        void start() override;
+
+    private:
+        /**
+         * @brief Function to be called by FreeRTOS function xTaskCreate().
+         * Calls setup() and loop()
+         *
+         * @param task WiFi Task object casted as void pointer
+         */
+        static void taskFunction(void *task);
+
+        /**
+         * @brief Setup WiFi Task before looping forever.
+         * This function should not be called outside of taskFunction()
+         *
+         */
+        void setup();
+
+        /**
+         * @brief Loop inside of WiFi Task.
+         * This function should not be called outside of taskFunction().
+         *
+         */
+        void loop();
+
+        /**
+         * @brief Connect to WiFi for first time
+         *
+         * @return true/false if successfully connected to WiFi or not
+         */
+        bool setupWifi();
+    };
+
+}
+
+#endif
