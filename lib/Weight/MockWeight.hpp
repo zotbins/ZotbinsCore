9fb070eeea6d7621bf05e3670b885b046844a635
Emil Zublin <ejzublin@gmail.com>
2022-04-28 16:27:12 -0700

    Added comments to the mock weight class >> >> Co-authored by: Derek Skinner 60554721+dskin344@users.noreply.github.com

--- lib/Weight/MockWeight.hpp
+++ lib/Weight/MockWeight.hpp
@@ -1,12 +1,31 @@
 #include "IWeight.hpp"
 
+/**
+ * @brief Mock weight class that inherits from the IWeight interface class.
+ * Used for unit tests so that actual hardware isn't require to test the ZotBins
+ * system.
+ */
+
 class MockWeight : IWeight
 {
 public:
+    /**
+     * @brief Constructs an instance of the MockWeight class.
+     * @param mWeight Represents the current weight of the mock sensor.
+     *  
+     */
     MockWeight(int32_t mWeight);
 
+    /** 
+     * @brief Returns the mock weight.
+     * @return int32_t
+     */
     int32_t getWeight() override;
 
 private:
+    /**
+     * @brief Represents the current weight of the mock sensor.
+     * 
+     */
     int32_t mWeight;
 };
