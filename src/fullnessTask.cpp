c66f7907d0422d09fe04eae3d9da2c792539bd3a
aahsue <abahsue@gmail.com>
2022-05-24 19:00:40 -0700

    interfaced fullness task with ESP-32 and ultrasonic sensor
    Co-authored-by: Lily Johnson <41386414+lilyjohnson@users.noreply.github.com>

--- src/fullnessTask.cpp
+++ src/fullnessTask.cpp
@@ -1,12 +1,19 @@
 #include "fullnessTask.hpp"
 #include "FullnessMetric.hpp"
+#include "MockDistance.hpp"
 #include <Arduino.h>
+#include <vector>
 
 using namespace Zotbins;
 
 static const char *name = "fullnessTask";
 static const int priority = 1;
 static const uint32_t stackSize = 4096;
+
+const int trigPin = 5;
+const int echoPin = 18;
+int amount = 0;
+std::vector<int32_t> distances;
 
 FullnessTask::FullnessTask(QueueHandle_t &messageQueue)
     : Task(name, priority, stackSize), mMessageQueue(messageQueue)
@@ -27,13 +34,46 @@ void FullnessTask::taskFunction(void *task)
 
 void FullnessTask::setup()
 {
+    Serial.begin(115200);
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
 }
 
 void FullnessTask::loop()
 {
     while (1)
     {
-        vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay for 1000 milliseconds
-        log_i("Hello from Fullness Task");
+        if (amount < 8)
+        {
+            digitalWrite(trigPin, LOW);
+            vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay for 1000 milliseconds
+            digitalWrite(trigPin, HIGH);
+            vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay for 1000 milliseconds
+            digitalWrite(trigPin, LOW);
+
+            // read echo pin
+            long duration = pulseIn(echoPin, HIGH);
+            int32_t intDuration = duration * .034 / 2;
+            log_i("Ultrasonic sensor reading: %d", intDuration);
+
+            // adding to vector
+            distances.push_back(duration);
+            amount++;
+        }
+        else
+        {
+            Fullness::MockDistance distanceSensor(distances);
+            Fullness::FullnessMetric fullness(1000, distanceSensor);
+
+            log_i("Fullness: %f", fullness.getFullness());
+            amount = 0;
+        }
+
+        // log_i("Hello from Fullness Task");
+
+        // distance sensor
+        // Fullness::IDistance distanceSensor;
+
+        // log_i();
     }
 }
