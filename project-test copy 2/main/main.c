10e4946be6b7f21b02e78a5cd90f86b205bd60a9
anthony25862 <anthc15@uci.edu>
2024-10-31 16:09:46 -0700

    Deleted weird copy + added stream code
    Title

--- project-test copy 2/main/main.c
+++ project-test copy 2/main/main.c
@@ -247,6 +247,80 @@ void save_image_to_json(const char *encoded_image) {
     cJSON_Delete(json);
 }
 
+// Code segment taken from ESP32 Camera driver
+#define PART_BOUNDARY "123456789000000000000987654321"
+static const char* _STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
+static const char* _STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
+static const char* _STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
+esp_err_t jpg_stream_httpd_handler(httpd_req_t *req){
+    camera_fb_t * fb = NULL;
+    esp_err_t res = ESP_OK;
+    size_t _jpg_buf_len;
+    uint8_t * _jpg_buf;
+    char * part_buf[64];
+    static int64_t last_frame = 0;
+    if(!last_frame) {
+        last_frame = esp_timer_get_time();
+    }
+
+    res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
+    if(res != ESP_OK){
+        return res;
+    }
+
+    while(true){
+        fb = esp_camera_fb_get();
+        if (!fb) {
+            ESP_LOGE(TAG, "Camera capture failed");
+            res = ESP_FAIL;
+            break;
+        }
+        if(fb->format != PIXFORMAT_JPEG){
+            bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
+            if(!jpeg_converted){
+                ESP_LOGE(TAG, "JPEG compression failed");
+                esp_camera_fb_return(fb);
+                res = ESP_FAIL;
+            }
+        } else {
+            _jpg_buf_len = fb->len;
+            _jpg_buf = fb->buf;
+        }
+
+        if(res == ESP_OK){
+            res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
+        }
+        if(res == ESP_OK){
+            size_t hlen = snprintf((char *)part_buf, 64, _STREAM_PART, _jpg_buf_len);
+
+            res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
+        }
+        if(res == ESP_OK){
+            res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
+        }
+        if(fb->format != PIXFORMAT_JPEG){
+            free(_jpg_buf);
+        }
+        esp_camera_fb_return(fb);
+        if(res != ESP_OK){
+            break;
+        }
+        int64_t fr_end = esp_timer_get_time();
+        int64_t frame_time = fr_end - last_frame;
+        last_frame = fr_end;
+        frame_time /= 1000;
+        // ESP_LOGI(TAG, "MJPG: %uKB %ums (%.1ffps)",
+        //     (uint32_t)(_jpg_buf_len/1024),
+        //     (uint32_t)frame_time, 1000.0 / (uint32_t)frame_time);
+    }
+
+    last_frame = 0;
+    return res;
+}
+
+
+
+
 // void init_spiffs() {
 //     esp_vfs_spiffs_conf_t conf = {
 //         .base_path = "/spiffs",
@@ -277,13 +351,6 @@ void save_image_to_json(const char *encoded_image) {
 // }
 
 //EVERYTHING BELOW THIS are http fxns (except app_main is simply to view image for quality check)
-
-
-
-
-
-#include "esp_camera.h"
-#include "esp_http_server.h"
 
 typedef struct {
         httpd_req_t *req;
@@ -337,10 +404,6 @@ esp_err_t jpg_httpd_handler(httpd_req_t *req){
 }
 
 
-
-
-
-
 // // HTTP GET handler for serving the captured image
 // esp_err_t capture_handler(httpd_req_t *req) {
 //     // Capture image from camera
@@ -381,7 +444,7 @@ void start_camera_server() {
         httpd_uri_t capture_uri = {
             .uri       = "/capture",
             .method    = HTTP_GET,
-            .handler   = jpg_httpd_handler,
+            .handler   = jpg_stream_httpd_handler, // jpg_httpd_handler,
             .user_ctx  = NULL
         };
 
