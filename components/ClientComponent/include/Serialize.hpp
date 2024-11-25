101c5b87515dad72c13ff652e7552f5840715503
Eric L <eliu15@uci.edu>
2024-11-25 02:31:24 -0800

    Add very basic serialization subroutine with cJSON

---
+++ components/ClientComponent/include/Serialize.hpp
@@ -0,0 +1,22 @@
+#ifndef CLIENT_SERIALIZE_HPP
+#define CLIENT_SERIALIZE_HPP
+
+#include <cJSON.h>
+#include <stdint.h>
+
+namespace Client {
+    /*
+     * @brief Serialize data from sensors for API interaction.
+     *
+     * The current shape of the JSON object returned is the following:
+     * {
+     *   "fullness": 1,
+     *   "weight": 1
+     * }
+     *
+     * @return cJSON* root of a JSON object containing sensor data. Make sure to free this after you are done using it.
+     */
+    cJSON* serialize(float fullness, int32_t weight);
+}
+
+#endif
