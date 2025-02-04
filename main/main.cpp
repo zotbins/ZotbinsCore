f0de32f73a042d1aa12237ebbadffbab63a20caa
Ryan Su <ryan.parkcity@gmail.com>
2025-02-03 16:59:34 -0800

    Code compiles, still need to test

--- main/main.cpp
+++ main/main.cpp
@@ -1,7 +1,7 @@
-#include "ESP32_VL53L0X.h" // Include the VL53L0X library
 #include "driver/i2c.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "vl53l0x.h" // Include VL53L0X library
 #include <stdio.h>
 
 #define I2C_MASTER_SCL_IO 15      /*!< GPIO number for I2C master clock */
@@ -9,7 +9,9 @@
 #define I2C_MASTER_NUM I2C_NUM_0  /*!< I2C port number for master dev */
 #define I2C_MASTER_FREQ_HZ 100000 /*!< I2C master clock frequency */
 
-VL53L0X sensor;
+#define XSHUT_PIN -1             /*!< Set to -1 if not using XSHUT */
+#define VL53L0X_I2C_ADDRESS 0x29 /*!< Default I2C address for VL53L0X */
+#define IO_2V8 0                 /*!< Set to 1 if using 2.8V logic, 0 if using 3.3V/5V */
 
 void i2c_master_init()
 {
@@ -24,37 +26,26 @@ void i2c_master_init()
     ESP_ERROR_CHECK(i2c_driver_install(I2C_MASTER_NUM, I2C_MODE_MASTER, 0, 0, 0));
 }
 
-extern "C" void app_main()
+extern "C" void app_main(void)
 {
-    printf("Starting VL53L0X sensor example\n");
-
     // Initialize I2C
     i2c_master_init();
 
+    vl53l0x_t *sensor = vl53l0x_config(I2C_MASTER_NUM, I2C_MASTER_SCL_IO, I2C_MASTER_SDA_IO, XSHUT_PIN, VL53L0X_I2C_ADDRESS, IO_2V8);
+
+    printf("Starting VL53L0X sensor example\n");
+
     // Initialize the VL53L0X sensor
-    if (!sensor.begin())
+    if (vl53l0x_init(sensor) == NULL)
+    {
+        printf("VL53L0X sensor initialized successfully\n");
+    }
+    else
     {
         printf("Failed to initialize VL53L0X\n");
-        return;
     }
 
-    printf("VL53L0X sensor initialized successfully\n");
+    return;
 
     // Loop to read the distance and print it
-    while (true)
-    {
-        // Read distance in millimeters
-        uint16_t distance = sensor.readRangeSingleMillimeters();
-        if (sensor.timeoutOccurred())
-        {
-            printf("Sensor timeout occurred!\n");
-        }
-        else
-        {
-            printf("Distance: %d mm\n", distance);
-        }
-
-        // Delay before the next reading
-        vTaskDelay(500 / portTICK_PERIOD_MS);
-    }
 }
