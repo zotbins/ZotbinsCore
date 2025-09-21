f3284630b6e639da8dcfe235b7ef5fc8c44f7314
Youngwoo Chang <youngwc7@uci.edu>
2025-09-20 17:21:20 -0700

    initial commit -- branching to incorporate camera sensor

---
+++ components/peripherals/peripheral_interface/camera_sensor.cpp
@@ -0,0 +1,10 @@
+#include "camera_sensor.hpp"
+#include "esp_log.h"
+
+static const char* TAG = "camera_sensor";
+
+esp_err_t init_camera(void) {
+    ESP_LOGI(TAG, "Camera initialization not yet implemented.");
+    return ESP_OK;
+}
+
---
+++ components/peripherals/peripheral_interface/include/camera_sensor.hpp
@@ -0,0 +1,8 @@
+#ifndef CAMERA_SENSOR_HPP
+#define CAMERA_SENSOR_HPP
+
+#include "esp_err.h"
+
+esp_err_t init_camera(void);
+
+#endif // CAMERA_SENSOR_HPP
